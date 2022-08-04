face1768064be5ddab0b791a8c98293d5dd71905
Correa Yancel <lacorrea@indracompany.com>
2022-08-04 18:09:00 -0500

    creando componentes

---
+++ Components+/scripts/loader.js
@@ -0,0 +1,8 @@
+﻿
+define(['jquery', 'knockout'], function ($, ko)
+{
+   ko.components.register('usersignup', {
+        require: 'scripts/usersignup'
+    });
+    ko.applyBindings();
+});
---
+++ Components+/scripts/usersignup.js
@@ -0,0 +1,14 @@
+﻿define(['knockout', 'text!../usersignup.htm'], function (ko, usersignupTemplate) {
+    function usersignup(params) {
+        var self = this;
+        self.firstname = ko.observable(params.firstname),
+        self.lastname = ko.observable(params.lastname),
+        self.email = ko.observable(params.email),
+        self.password = ko.observable(),
+        self.fullname = ko.pureComputed(function () {
+            return self.firstname() + " " + self.lastname()
+        })
+        return self;
+    }
+    return { viewModel: usersignup, template: usersignupTemplate };
+});
