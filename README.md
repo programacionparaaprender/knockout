6a17813771d7980ba78807c64db99d7311e77240
Correa Yancel <lacorrea@indracompany.com>
2022-08-05 10:05:59 -0500

    agregando cambios

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 https://www.udemy.com/course/master-knockoutjs-javascript/
 
 
+### crear proyecto vacio en .net
+https://stackoverflow.com/questions/58224003/how-to-create-an-asp-net-website-in-visual-studio-2019
+
 ### remove items
 https://knockoutjs.com/documentation/click-binding.html
 
---
+++ WebSite/WebSite/scripts/template-viewmodel.js
@@ -0,0 +1,15 @@
+﻿var userSignupModel = function (params) {
+
+    firstname = ko.observable(params.firstname),
+    lastname = ko.observable(params.lastname),
+    email = ko.observable(params.email),
+    password = ko.observable(),
+    fullname = ko.pureComputed(function () {
+        return this.firstname() + " " + this.lastname()
+    })
+
+};
+
+ko.components.register('usersignup', {
+    template: { element: 'signup-template' },
+});
